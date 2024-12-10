0866193bd35590b0080f421bdf316f92956d7976
mmhct <12211026@mail.sustech.edu.cn>
2024-12-10 14:14:08 +0800

    成功做出将目前五个改进方向全部实现的v3.5版本

--- auto_scan_v3.5.py
+++ auto_scan_v3.5.py
@@ -7,21 +7,21 @@ import time
 from auto_scan import run
 
 
-def perform_task(have,iter):
+def perform_task(have, iter):
     start = time.time()
     # 继续执行剩余的代码
     pyautogui.click(386, 939)  # 点击5号场地
-    #time.sleep(1.5)  # 等待一下，这里必须考虑来自服务器的延迟
+    # time.sleep(1.5)  # 等待一下，这里必须考虑来自服务器的延迟
     pyautogui.click(29, 83)  # 点击系统浏览器回退键
     if iter == 0:
         pyautogui.click(309, 302)  # 点击1号场地
     elif iter == 1:
-        #pyautogui.click(354, 1360) # 点击7号场地
+        # pyautogui.click(354, 1360) # 点击7号场地
         pyautogui.click(395, 515)  # 点击2号场地
     elif iter == 2:
-        pyautogui.click(395, 515) # 点击2号场地
+        pyautogui.click(395, 515)  # 点击2号场地
     elif iter == 3:
-        pyautogui.click(351, 742) # 点击3号场地
+        pyautogui.click(351, 742)  # 点击3号场地
 
     if iter == 0:
         time.sleep(1.5)
@@ -29,7 +29,7 @@ def perform_task(have,iter):
         time.sleep(1)
     else:
         time.sleep(0.5)
-    #time.sleep(1.5)  # 等待一下,这里必须考虑来自服务器的延迟，保险起见设置>3秒
+    # time.sleep(1.5)  # 等待一下,这里必须考虑来自服务器的延迟，保险起见设置>3秒
     pyautogui.click(2239, 518)  # 点击日期框
     pyautogui.click(1284, 1409)  # 这个是选择下一个月份，如果遇到月份更替就要做这个操作，如果没有更替做这个也没事
     # 这个月份功能还没测试，不知道是否能用，因为选择月份之后可能会有些系统延迟，需要等待一下，等月份更替的时候进行测试
@@ -49,42 +49,48 @@ def perform_task(have,iter):
     time.sleep(0.18)  # 等待一下,如果这里还要强行优化，那就识别蒙上一层黑雾的图片
     detection_results = run()
     detection_results = {key: not value for key, value in detection_results.items()}
-    if (detection_results[14] and detection_results[17]) and (14 not in have and 15 not in have and 16 not in have and 17 not in have):
+    if (detection_results[14] and detection_results[17]) and (
+            14 not in have and 15 not in have and 16 not in have and 17 not in have):
         pyautogui.click(1424, 715)  # 15-15:30
         pyautogui.click(2174, 720)  # 16:30-17
         have.append(14)
         have.append(15)
         have.append(16)
         have.append(17)
-    elif (detection_results[15] and detection_results[18]) and (15 not in have and 16 not in have and 17 not in have and 18 not in have):
+    elif (detection_results[15] and detection_results[18]) and (
+            15 not in have and 16 not in have and 17 not in have and 18 not in have):
         pyautogui.click(1667, 724)  # 15:30-16
         pyautogui.click(147, 816)  # 17-17:30
         have.append(15)
         have.append(16)
         have.append(17)
         have.append(18)
-    elif (detection_results[16] and detection_results[19]) and (16 not in have and 17 not in have and 18 not in have and 19 not in have):
+    elif (detection_results[16] and detection_results[19]) and (
+            16 not in have and 17 not in have and 18 not in have and 19 not in have):
         pyautogui.click(1926, 722)  # 16-16:30
         pyautogui.click(408, 815)  # 17:30-18
         have.append(16)
         have.append(17)
         have.append(18)
         have.append(19)
-    elif (detection_results[12] and detection_results[15]) and (12 not in have and 13 not in have and 14 not in have and 15 not in have):
+    elif (detection_results[12] and detection_results[15]) and (
+            12 not in have and 13 not in have and 14 not in have and 15 not in have):
         pyautogui.click(905, 722)  # 14-14:30
         pyautogui.click(1667, 724)  # 15:30-16
         have.append(12)
         have.append(13)
         have.append(14)
         have.append(15)
-    elif (detection_results[13] and detection_results[16]) and (13 not in have and 14 not in have and 15 not in have and 16 not in have):
+    elif (detection_results[13] and detection_results[16]) and (
+            13 not in have and 14 not in have and 15 not in have and 16 not in have):
         pyautogui.click(1164, 721)  # 14:30-15
         pyautogui.click(1926, 722)  # 16-16:30
         have.append(13)
         have.append(14)
         have.append(15)
         have.append(16)
-    elif (detection_results[3] and detection_results[6]) and (3 not in have and 4 not in have and 5 not in have and 6 not in have):
+    elif (detection_results[3] and detection_results[6]) and (
+            3 not in have and 4 not in have and 5 not in have and 6 not in have):
         pyautogui.click(900, 622)  # 9:30-10
         pyautogui.click(1657, 627)  # 11-11:30
         have.append(3)
@@ -102,22 +108,21 @@ def perform_task(have,iter):
 
     # 下面的固定不改
     pyautogui.click(215, 1192)  # 点击使用人数框
-    #pyautogui.click(240, 1252)  # 篮球场测试用
+    # pyautogui.click(240, 1252)  # 篮球场测试用
     time.sleep(0.15)  # 等待一下
     pyautogui.write("1")  # 输入文本
     pyautogui.click(714, 1245)  # 点击预约
     end = time.time()
     print("任务执行完毕，耗时：", end - start)
-    #pyautogui.click(629, 1301) # 篮球场测试用
+    # pyautogui.click(629, 1301) # 篮球场测试用
     time.sleep(1)
     pyautogui.click(29, 83)  # 点击系统浏览器回退键
     pyautogui.click(29, 83)  # 点击系统浏览器回退键,要点两次，因为点一次是回退到场地界面，再点一次是回退到主界面
     return have
 
 
-
 if __name__ == "__main__":
-    set_time = datetime.datetime.strptime("2024-12-06 21:45:00", "%Y-%m-%d %H:%M:%S")
+    set_time = datetime.datetime.strptime("2024-12-10 20:00:01", "%Y-%m-%d %H:%M:%S")
     time_difference = (set_time - datetime.datetime.now()).total_seconds()
     while time_difference > 0:
         if time_difference > 10:
@@ -126,5 +131,5 @@ if __name__ == "__main__":
             time.sleep(0.1)
         time_difference = (set_time - datetime.datetime.now()).total_seconds()
     have = []
-    for i in range(2):
-        have=perform_task(have,i)  # 执行任务
+    for i in range(4):
+        have = perform_task(have, i)  # 执行任务
